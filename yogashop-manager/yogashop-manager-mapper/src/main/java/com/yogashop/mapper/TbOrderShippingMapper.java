26718fec9427d413f9adfba11b281fc86546add5
danelumax <474198699@qq.com>
2017-03-14 12:39:00 +0800

    YogaShop : addd mybatis generator pojo and  mapper

---
+++ yogashop-manager/yogashop-manager-mapper/src/main/java/com/yogashop/mapper/TbOrderShippingMapper.java
@@ -0,0 +1,30 @@
+package com.yogashop.mapper;
+
+import com.yogashop.pojo.TbOrderShipping;
+import com.yogashop.pojo.TbOrderShippingExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderShippingMapper {
+    int countByExample(TbOrderShippingExample example);
+
+    int deleteByExample(TbOrderShippingExample example);
+
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrderShipping record);
+
+    int insertSelective(TbOrderShipping record);
+
+    List<TbOrderShipping> selectByExample(TbOrderShippingExample example);
+
+    TbOrderShipping selectByPrimaryKey(String orderId);
+
+    int updateByExampleSelective(@Param("record") TbOrderShipping record, @Param("example") TbOrderShippingExample example);
+
+    int updateByExample(@Param("record") TbOrderShipping record, @Param("example") TbOrderShippingExample example);
+
+    int updateByPrimaryKeySelective(TbOrderShipping record);
+
+    int updateByPrimaryKey(TbOrderShipping record);
+}
